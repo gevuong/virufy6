0dc678f067f8bd9e2ad3e69584ef485ca940af72
gevuong <gevuong5627@gmail.com>
2024-04-03 16:21:12 -0700

    [test]: comment out i18n and rewrites function in next config

--- next.config.mjs
+++ next.config.mjs
@@ -4,26 +4,30 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
  * This is especially useful for Docker builds.
  */
-!process.env.SKIP_ENV_VALIDATION && (await import("./src/env.mjs"));
+!process.env.SKIP_ENV_VALIDATION && (await import('./src/env.mjs'))
 
 /** @type {import("next").NextConfig} */
-export default {
+const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
-  i18n: {
-    locales: ["en", "es", "pt", "jp"],
-    defaultLocale: "en",
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*', // automatically handles all locales
-        destination: 'https://scidroidgames-gmailcom-cms.payloadcms.app/api/:path*', // automatically passes the locale on
-      },
-      {
-        source: '/media/:path*', // automatically handles all locales
-        destination: 'https://scidroidgames-gmailcom-cms.payloadcms.app/media/:path*', // automatically passes the locale on
-      },
-    ]
-
-  }
+  // i18n: {
+  //   locales: ['en', 'es', 'pt', 'jp'],
+  //   defaultLocale: 'en',
+  // },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/api/:path*', // automatically handles all locales
+  //       destination:
+  //         'https://scidroidgames-gmailcom-cms.payloadcms.app/api/:path*', // automatically passes the locale on
+  //     },
+  //     {
+  //       source: '/media/:path*', // automatically handles all locales
+  //       destination:
+  //         'https://scidroidgames-gmailcom-cms.payloadcms.app/media/:path*', // automatically passes the locale on
+  //     },
+  //   ]
+  // },
 }
+
+export default nextConfig
