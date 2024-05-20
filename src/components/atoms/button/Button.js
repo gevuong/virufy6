78217545f393ff68099af4272275354377328fca
ridham-dd <147288551+ridham-dd@users.noreply.github.com>
2024-05-19 22:19:07 -0500

    updated button atom

--- src/components/atoms/button/Button.js
+++ src/components/atoms/button/Button.js
@@ -1,16 +1,13 @@
 import Link from "next/link";
 import { ButtonType, ButtonSize } from "../../themes";
 
-export default function Button1({ size, type, label, style, radius, gradientBackground, path, width, height, onClick = () => { }}) {
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+export default function Button({ size, type, label, path, onClick = () => { } }) {
+
     return (
-        <Link href={path}>
-            <button
-                onClick={onClick}
-                className={`${ButtonType?.[type]} ${ButtonSize?.[size]} ${style}`}
-                style={{ background: gradientBackground, borderRadius: radius, width: width, height: height }}
-            >
-                {label}
-            </button>
-        </Link>
+        <>
+
+            <button onClick={onClick} className={`${ButtonType?.[type]} ${ButtonSize?.[size]}`}><Link href={path}>{label?.toUpperCase()}</Link></button>
+        </>
     )
 }
