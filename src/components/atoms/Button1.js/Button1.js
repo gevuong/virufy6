4ba7cb7f828aa3b0cf177086e4865a6276430e95
ridham-dd <147288551+ridham-dd@users.noreply.github.com>
2024-05-19 22:42:12 -0500

    The button was clickable only on text, Now the entire button is clickable

--- src/components/atoms/Button1.js/Button1.js
+++ src/components/atoms/Button1.js/Button1.js
@@ -5,16 +5,14 @@ import { ButtonType, ButtonSize } from "../../themes";
 export default function Button1({ size, type, label, style, radius, gradientBackground, path, width, height, onClick = () => { }}) {
     return (
         <>
+        <Link href={path}>
             <button
                 onClick={onClick}
                 className={`${ButtonType?.[type]} ${ButtonSize?.[size]} ${style}`} // Removed the extra style prop
-                style={{ background: gradientBackground, borderRadius: radius, width: width, height: height }}
-            >
-                {/* <Link href={path}>{label?.toUpperCase()}</Link> */}
-                <Link href={path}>
+                style={{ background: gradientBackground, borderRadius: radius, width: width, height: height }}>
                     {label}
-                </Link>
             </button>
+            </Link>
         </>
     )
 }
