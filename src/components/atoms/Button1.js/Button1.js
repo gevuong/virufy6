2a3c5a517fc7735b00bdab69bccd3094926ff325
ridham-virufy <165954957+ridham-virufy@users.noreply.github.com>
2024-06-06 17:26:29 +0000

    Revert "section 1 of the homepage"
    This reverts commit 2502e4d0f352a553f1361e5c8fd9220e79b00b76.

--- src/components/atoms/Button1.js/Button1.js
+++ src/components/atoms/Button1.js/Button1.js
@@ -1,20 +1,18 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import Link from "next/link";
+import { ButtonType, ButtonSize } from "../../themes";
 
-const Button1 = ({ size, type, path, label, radius, gradientBackground, width, height, className }) => {
-  return (
-    <button
-      className={`${className} ${size} ${type}`}
-      style={{
-        borderRadius: radius,
-        background: gradientBackground,
-        width: width,
-        height: height,
-      }}
-      onClick={() => window.location.href = path}
-    >
-      {label}
-    </button>
-  );
-};
-export default Button1;
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+export default function Button1({ size, type, label, style, radius, gradientBackground, path, width, height, onClick = () => { }}) {
+    return (
+        <>
+        <Link href={path}>
+            <button
+                onClick={onClick}
+                className={`${ButtonType?.[type]} ${ButtonSize?.[size]} ${style}`} // Removed the extra style prop
+                style={{ background: gradientBackground, borderRadius: radius, width: width, height: height }}>
+                    {label}
+            </button>
+            </Link>
+        </>
+    )
+}
