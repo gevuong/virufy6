214d728b6e06b908e19feda08128aab373aab92c
gevuong <gevuong5627@gmail.com>
2024-07-02 22:02:49 -0700

    [fix-build]: comment out i18n and rewrites in next config

--- next.config.mjs
+++ next.config.mjs
@@ -9,24 +9,25 @@
 /** @type {import("next").NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  i18n: {
-    locales: ['en', 'es', 'pt', 'jp'],
-    defaultLocale: 'en',
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*', // automatically handles all locales
-        destination:
-          'https://scidroidgames-gmailcom-cms.payloadcms.app/api/:path*', // automatically passes the locale on
-      },
-      {
-        source: '/media/:path*', // automatically handles all locales
-        destination:
-          'https://scidroidgames-gmailcom-cms.payloadcms.app/media/:path*', // automatically passes the locale on
-      },
-    ]
-  },
+  output: 'export',
+  // i18n: {
+  //   locales: ['en', 'es', 'pt', 'jp'],
+  //   defaultLocale: 'en',
+  // },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/api/:path*', // automatically handles all locales
+  //       destination:
+  //         'https://scidroidgames-gmailcom-cms.payloadcms.app/api/:path*', // automatically passes the locale on
+  //     },
+  //     {
+  //       source: '/media/:path*', // automatically handles all locales
+  //       destination:
+  //         'https://scidroidgames-gmailcom-cms.payloadcms.app/media/:path*', // automatically passes the locale on
+  //     },
+  //   ]
+  // },
 }
 
 export default nextConfig
