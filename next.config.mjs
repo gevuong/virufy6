8d2b82765b87620b996c690733be376d50d6941d
gevuong <gevuong5627@gmail.com>
2024-07-02 22:23:55 -0700

    add base path

--- next.config.mjs
+++ next.config.mjs
@@ -5,12 +5,20 @@
  * This is especially useful for Docker builds.
  */
 !process.env.SKIP_ENV_VALIDATION && (await import('./src/env.mjs'))
+const isProd = process.env.NODE_ENV === 'production'
 
 /** @type {import("next").NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
   distDir: 'out',
+  /**
+   * Set base path. This is the slug of your GitHub repository.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+   */
+  basePath: isProd ? '/virufy6' : '',
   // i18n: {
   //   locales: ['en', 'es', 'pt', 'jp'],
   //   defaultLocale: 'en',
